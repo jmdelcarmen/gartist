9ff9a50f8fa2b79301c7497be1a3010aee3fdd4c
Marco del Carmen <jesusmarcodelcarmen@gmail.com>
2017-04-22 00:24:14 -0500

    Remake

--- src/components/app.js
+++ src/components/app.js
@@ -1,60 +1,7 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { markdown } from 'markdown';
+import React from 'react';
 
-import SearchBox from './search-box';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      q: '',
-      setlist: [],
-      searchResults: [],
-      currentSongLyrics: ''
-    }
-  }
-  submitSearch = e => {
-    e.preventDefault();
-    axios.post('http://localhost:3000/search', { q: this.state.q })
-      .then(({ data }) => this.setState({ searchResults: data }))
-  }
-  getLyrics = e => {
-    e.preventDefault();
-    const songId = e.target.href.replace('http://localhost:8080/', '');
-    axios.post('http://localhost:3000/lyrics', { songId })
-      .then(data => {
-        this.setState({ currentSongLyrics: data.data });
-      });
-  }
-  renderSearchResults = () => this.state.searchResults.map(hit => {
-    return (
-      <li key={hit.result.id}>
-        <span>{hit.result.full_title}</span>
-        <br></br>
-        <a
-          onClick={this.getLyrics}
-          href={hit.result.id}>View Lyrics</a>
-      </li>
-    );
-  });
-  render() {
-    const content = markdown.toHTML(this.state.currentSongLyrics);
-    return (
-      <div>
-        <SearchBox
-          onSubmit={this.submitSearch}
-          onChange={e => this.setState({ q: e.target.value })}/>
-        <div className="row">
-          <div className="col-md-4">
-            <ul>{this.renderSearchResults()}</ul>
-          </div>
-          <div className="col-md-8" dangerouslySetInnerHTML={{ __html: content }}>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <h1>Hello World!</h1>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
 import thunk from 'redux-thunk';
-import App from './components/app';
 import reducers from './reducers';
+import Routes from './routes';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} routes={Routes}/>
   </Provider>
   , document.querySelector('.container'));
