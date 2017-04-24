69fcd7c54e5536fb59450db235dea67c146a0208
Marco del Carmen <jesusmarcodelcarmen@gmail.com>
2017-04-24 02:08:50 -0500

    Added owner and name to setlist model

--- src/server/models/setlist/index.js
+++ src/server/models/setlist/index.js
@@ -2,15 +2,17 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema
 
 const setlistSchema = new Schema({
-  artist: { type: Schema.ObjectId, ref: 'Artist' },
-  performanceDate: Date,
+  name: String,
   comment: String,
+  thumbnailUrl: String,
+  performanceDate: Date,
   venue: {
     name: String,
     city: String,
     state: String,
   },
-  thumbnailUrl: String,
+  ownerId: { type: Schema.ObjectId, ref: 'User' },
+  artist: { type: Schema.ObjectId, ref: 'Artist' },
   songs: [{ type: Schema.ObjectId, ref: 'Song' }],
 }, { timestamps: true });
 
